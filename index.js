e0c6c5fe9011c9aa6f23ae83d6db28caf03ba14b
estebangzcs-collab <estebangzcs@gmail.com>
2026-08-06 20:03:16 -0500

    node.clear

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req, "node"res) => {
   res.send('Servidor Express funcionando!');
 });
 
